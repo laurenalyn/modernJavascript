961fce55f34290b7ad4b0653edc6815e2bf45799
laurenalyn <laurenalynwilkerson@gmail.com>
2019-05-16 11:54:37 -0400

    data types

--- JavaScriptLanguageFundamentals/app.js
+++ JavaScriptLanguageFundamentals/app.js
@@ -1,72 +1,38 @@
-// Log to console
-// console.log('Hello world!');
-// console.log(123);
-// console.log(true);
-// var greeting = "Hello";
-// console.log(greeting);
-// console.log([1,2,3,4]);
-// console.log({a:1, b:2});
-// console.table({a:1, b:2});
-// console.error('this is some error');
-// console.clear();
-// console.warn('this is a warning');
-// console.time('hello');
-//     console.log('hi');
-//     console.log('hi');
-//     console.log('hi');
-//     console.log('hi');
-//     console.log('hi');
-//     console.log('hi'); 
-// console.timeEnd('hello');
+// primitive types
+
+//string
+const name = 'John Doe';
+// number
+const age = 30;
+// boolean
+const hasKids = true;
+// null
+const car = null;
+// undefinded
+let test;
+// symbol
+const sym = Symbol();
+
+console.log(typeof name);
+console.log(typeof age);
+console.log(typeof hasKids);
+console.log(typeof car);
+console.log(typeof test);
+console.log(typeof sym);
+
+// referenct types - objects
+
+// array
+const hobbies = ['movies', 'music'];
+// object literal
+const address = {
+    city: 'boston',
+    state: 'ma'
+}
+// date
+const today = new Date();
 
 
-
-// // var, let, const
-// // var
-// var name = 'John Doe';
-// console.log(name);
-// name = 'Steve Smith';
-// console.log(name);
-
-// // init a variable
-// var greeting;
-// console.log(greeting);
-// greeting = 'hello';
-// console.log(greeting);
-
-// // letters, numbers, _, $ 
-// // can not start with a number
-// // multi word variables
-// var firstName; //camel case
-// var first_name; // underscore
-// var FirstName; // pascal case
-// var firstname;
-
-// // let
-
-// let name = 'John Doe';
-// console.log(name);
-// name = 'Steve Smith';
-// console.log(name);
-
-// const
-// const name = 'John';
-// console.log(name);
-// can not reassign
-// name = 'Sara';
-// have to assign a value
-// const greeting; 
-
-const person = {
-    name: 'John',
-    age: 30
-};
-
-person.name = 'Sara';
-person.age = 32;
-
-// console.log(person);
-
-const numbers = [1, 2, 3, 4];
-numbers.push(5);
-console.log(numbers);
+console.log(typeof hobbies);
+console.log(typeof address);
+console.log(typeof today);
