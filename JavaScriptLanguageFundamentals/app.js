c65f019ded8ffa3ad808173f615574c0317961a3
laurenalyn <laurenalynwilkerson@gmail.com>
2019-05-16 10:27:59 -0400

    playing in the console

--- JavaScriptLanguageFundamentals/app.js
+++ JavaScriptLanguageFundamentals/app.js
@@ -1,1 +1,21 @@
-// alert("Hello!");
+// Log to console
+console.log('Hello world!');
+console.log(123);
+console.log(true);
+var greeting = "Hello";
+console.log(greeting);
+console.log([1,2,3,4]);
+console.log({a:1, b:2});
+console.table({a:1, b:2});
+console.error('this is some error');
+console.clear();
+console.warn('this is a warning');
+console.time('hello');
+    console.log('hi');
+    console.log('hi');
+    console.log('hi');
+    console.log('hi');
+    console.log('hi');
+    console.log('hi');
+console.timeEnd('hello');
+
